4d9df72ab3920e9dd91a875515fa55294280065b
ArcNinjaC <121591277+ArcNinjaC@users.noreply.github.com>
2026-03-28 00:20:38 -0400

    more config + some basic setup

--- basic_grover_implementation/main.py
+++ basic_grover_implementation/main.py
@@ -14,9 +14,10 @@ num_qbits = 4
 # 2^n ? combinations of directions?
 
 world = [(-0.5,-0.5),(0.5,-0.5), (0.5,0.5),(-0.5,0.5)]
+world_border = 0.2
 
 # All rays are gonna have a constant y direction 1
-rays = [i/(num_qbits) - 0.5 for i in range(num_qbits)]
+rays = [i/(2**num_qbits-1) - 0.5 for i in range(2**num_qbits)]
 
 target = (0,0)
 ray_origin = (0,-0.5)
@@ -24,13 +25,40 @@ ray_origin = (0,-0.5)
 
 # CPU versions of code
 def raydata(dir):
-    pass 
+    t = 1
+    if dir != 0:
+        t = min(0.5/abs(dir),t)
+    return np.linspace(0, t, 50, True)
 
-def draw(dir):
+def draw(dir, ax):
     data = raydata(dir)
+    ax.plot(dir*data,data-0.5)
 
 def trace(dir):
     return None
+
+fig,ax = plt.subplots()
+
+ax.set_xlim((-0.5-world_border,0.5+world_border))
+ax.set_ylim((-0.5-world_border,0.5+world_border))
+
+rect = patches.Rectangle(
+    (-0.5,-0.5),
+    1,
+    1,
+    linewidth=2,
+    edgecolor='r',
+    facecolor='none' 
+)
+
+ax.add_patch(rect)
+
+# drawing
+for ray in rays:
+    draw(ray, ax)
+    pass
+
+plt.show()
 
 
 
