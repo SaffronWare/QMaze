5501408cb68d43ec24f1a398946ad878d6fe8ce2
ArcNinjaC <121591277+ArcNinjaC@users.noreply.github.com>
2026-03-28 01:18:11 -0400

    some basic skeleton functios + logic

--- basic_grover_implementation/main.py
+++ basic_grover_implementation/main.py
@@ -17,6 +17,7 @@ num_qbits = 4
 
 world = [(-0.5,-0.5),(0.5,-0.5), (0.5,0.5),(-0.5,0.5)]
 world_border = 0.2
+radius = 0.02
 
 # All rays are gonna have a constant y direction 1
 rays = [i/(2**num_qbits-1) - 0.5 for i in range(2**num_qbits)]
@@ -27,6 +28,7 @@ ray_origin = (0,-0.5)
 
 # Digital CPU versions of code
 class DigitalSolver:
+
     @staticmethod
     def raydata(dir):
         t = 1
@@ -43,7 +45,7 @@ class DigitalSolver:
             # should travel along x
             x_reach = ty*dir
 
-            if abs(x_reach - target[0]) <= 0.02:
+            if abs(x_reach - target[0]) <= radius:
                 out[0].append(i)
                 out[1].append(dir)
     
@@ -66,14 +68,14 @@ class DigitalSolver:
         )
 
         circle = patches.Circle(
-            target, 0.02
+            target, radius
         )
 
         ax.add_patch(rect)
         ax.add_patch(circle)
 
         i, dir_correct  = DigitalSolver.solve_ray(rays,ax)
-        print(i,dir_correct)
+        #print(i,dir_correct)
 
         for dir in rays:
             data = DigitalSolver.raydata(dir)
@@ -87,18 +89,44 @@ class DigitalSolver:
 
 class QuantumSolver:
     @staticmethod
-    def GetCircuit():
+    def Oracle(circuit):
         pass 
 
+    @staticmethod
+    def Diffuse(circuit):
+        pass
+    
+    @staticmethod
     def Grover():
+        num_grover_steps = 4
         # Initializes qubits
-        qc = QuantumCircuit(num_qbits)
+        qc = QuantumCircuit(num_qbits, num_qbits)
 
         # Places everything in an equal superposition
         # usin hadamard gate
         qc.h(range(num_qbits))
 
+        for i in range(num_grover_steps):
+            QuantumSolver.Oracle(qc)
+            QuantumSolver.Diffuse(qc)
+
+
+        # condition we want to check is if abs((target[1] + 0.5)*dir - target[0])) < radius
+        # target[1] + 0.5 is a cpu computed constant
+        # same for target[0]
+        # Now, id say we only need to worry about calculating dir first
+        # On cpu we did dir = i (qubit value ID) / (2**n_qubit - 1) - 0.5
+        # We can compute the constant 1/(2**n_qubit - 1) on digital circuit
+        # Call this K
+        K = 1/(2**num_qbits - 1)
+        # 0.5 also a constant
+
+
+        qc.measure(range(num_qbits), range(num_qbits))
+
+
         print(qc)
+        return qc
 
 
 
