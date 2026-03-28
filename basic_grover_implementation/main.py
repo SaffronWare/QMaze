045d0a8a916c80ebd9dff7bcc51097bc22765b39
ArcNinjaC <121591277+ArcNinjaC@users.noreply.github.com>
2026-03-28 01:05:46 -0400

    split into classes

--- basic_grover_implementation/main.py
+++ basic_grover_implementation/main.py
@@ -25,66 +25,85 @@ target = (random.uniform(-0.4,0.4),random.uniform(-0.4,0.4))
 ray_origin = (0,-0.5)
 
 
-# CPU versions of code
-def raydata(dir):
-    t = 1
-    if dir != 0:
-        t = min(0.5/abs(dir),t)
-    return np.linspace(0, t, 50, True)
-
-def findr(directions,ax):
-    out = [[],[]]
-    for i,dir in enumerate(directions):
-        # should travel along y
-        ty = target[1] + 0.5
-        # should travel along x
-        x_reach = ty*dir
-
-        if abs(x_reach - target[0]) <= 0.02:
-            out[0].append(i)
-            out[1].append(dir)
-        
-        s=0.5
-        #print(abs(ty-tx))
+# Digital CPU versions of code
+class DigitalSolver:
+    @staticmethod
+    def raydata(dir):
+        t = 1
+        if dir != 0:
+            t = min(0.5/abs(dir),t)
+        return np.linspace(0, t, 50, True)
     
-    return out
+    @staticmethod
+    def solve_ray(directions,ax):
+        out = [[],[]]
+        for i,dir in enumerate(directions):
+            # should travel along y
+            ty = target[1] + 0.5
+            # should travel along x
+            x_reach = ty*dir
+
+            if abs(x_reach - target[0]) <= 0.02:
+                out[0].append(i)
+                out[1].append(dir)
+    
+        return out
+
+    @staticmethod
+    def show_demo():
+        fig,ax = plt.subplots()
+        ax.set_aspect(1)
+        ax.set_xlim((-0.5-world_border,0.5+world_border))
+        ax.set_ylim((-0.5-world_border,0.5+world_border))
+
+        rect = patches.Rectangle(
+            (-0.5,-0.5),
+            1,
+            1,
+            linewidth=2,
+            edgecolor='r',
+            facecolor='none' 
+        )
+
+        circle = patches.Circle(
+            target, 0.02
+        )
+
+        ax.add_patch(rect)
+        ax.add_patch(circle)
+
+        i, dir_correct  = DigitalSolver.solve_ray(rays,ax)
+        print(i,dir_correct)
+
+        for dir in rays:
+            data = DigitalSolver.raydata(dir)
+            if dir not in dir_correct:
+                ax.plot(dir*data,data-0.5, c='green')
+            else:
+                ax.plot(dir*data, data-0.5, c='purple')
 
 
-fig,ax = plt.subplots()
-ax.set_aspect(1)
-ax.set_xlim((-0.5-world_border,0.5+world_border))
-ax.set_ylim((-0.5-world_border,0.5+world_border))
+        plt.show()
 
-rect = patches.Rectangle(
-    (-0.5,-0.5),
-    1,
-    1,
-    linewidth=2,
-    edgecolor='r',
-    facecolor='none' 
-)
+class QuantumSolver:
+    @staticmethod
+    def GetCircuit():
+        pass 
 
-circle = patches.Circle(
-    target, 0.02
-)
+    def Grover():
+        # Initializes qubits
+        qc = QuantumCircuit(num_qbits)
 
-ax.add_patch(rect)
-ax.add_patch(circle)
+        # Places everything in an equal superposition
+        # usin hadamard gate
+        qc.h(range(num_qbits))
 
-i, dir_correct  = findr(rays,ax)
-print(i,dir_correct)
-
-for dir in rays:
-    data = raydata(dir)
-    if dir not in dir_correct:
-        ax.plot(dir*data,data-0.5, c='green')
-    else:
-        ax.plot(dir*data, data-0.5, c='purple')
+        print(qc)
 
 
-plt.show()
 
-
+DigitalSolver.show_demo()
+QuantumSolver.Grover()
 
 
 
